fb97824180946b5e3b2ac8effc926942936bf6ae
CesarArellano <raywayday@gmail.com>
2023-02-02 00:22:32 -0600

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by César Mauricio Arellano Velásquez on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
